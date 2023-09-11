128f3d50ef16d7fed415eb4262bbfa0b32c26746
Krzysztof Ambroziak <krzysztof.amb@gmail.com>
2023-09-11 18:39:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KCinema
+
+Backend for cinema project wrote in Java + SpringBoot.
---
+++ src/main/java/com/github/krzysztofambroziak/kcinema/KCinemaApplication.java
@@ -0,0 +1,11 @@
+package com.github.krzysztofambroziak.kcinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KCinemaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KCinemaApplication.class, args);
+    }
+}
